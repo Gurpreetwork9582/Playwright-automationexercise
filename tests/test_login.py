81cf1e8ebf00ecf0050f9f7d196e41313870b801
Gurpreetwork9582 <gurpreet.singh@bmmca.internal>
2026-04-30 14:59:45 -0300

    Test cases

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,8 +11,16 @@ def test_login(page: Page):
     response = requests.get('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
     assert response.status_code == 200
     
+    
 @pytest.mark.xfail
-def Wrong_test_login(page:Page):
+def test_Wrong_login(page:Page):
     page.get_by_placeholder('username').fill("Admin234")
     page.get_by_placeholder('Password').fill('admin123341')
     page.get_by_role("button", name="Login").click()
+
+
+def test_logout(logged_in:Page):
+    logged_in.get_by_alt_text("profile picture").click()
+    logged_in.get_by_text("Logout")
+    response = requests.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    assert response.status_code == 200
